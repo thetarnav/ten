f177d9a0764e16b65571e79202e11bb1c706f71f
Damian Tarnawski <gthetarnav@gmail.com>
2025-04-13 16:12:14 +0200

    Change indent to space 4

--- main.ts
+++ main.ts
@@ -8,59 +8,59 @@ const len = (a: ArrayLike<any>): number => a.length
 
 export
 enum Token_Kind {
-	/*
-		Special Tokens
-	*/
-	/**             */ Invalid,
-	/** end of file */ EOF,
-	/** \n          */ EOL,
-	/*
-		Symbol Tokens
-	*/
-	/** ?           */ Question,
-	/** !           */ Negative,
-	/** |           */ Or,
-	/** &           */ And,
-	/** =           */ Eq,
-	/** +           */ Add,
-	/** -           */ Sub,
-	/** *           */ Mul,
-	/** /           */ Div,
-	/** ^           */ Pow,
-	/** @           */ At,
-	/** "           */ Quote,
-	/** (           */ Paren_L,
-	/** )           */ Paren_R,
-	/*
-		Long Tokens
-	*/
-	/** "<string>"  */ String,
-	/** foo         */ Ident,
-	/** 123         */ Int,
-	/** 123.123     */ Float,
+    /*
+        Special Tokens
+    */
+    /**             */ Invalid,
+    /** end of file */ EOF,
+    /** \n          */ EOL,
+    /*
+        Symbol Tokens
+    */
+    /** ?           */ Question,
+    /** !           */ Negative,
+    /** |           */ Or,
+    /** &           */ And,
+    /** =           */ Eq,
+    /** +           */ Add,
+    /** -           */ Sub,
+    /** *           */ Mul,
+    /** /           */ Div,
+    /** ^           */ Pow,
+    /** @           */ At,
+    /** "           */ Quote,
+    /** (           */ Paren_L,
+    /** )           */ Paren_R,
+    /*
+        Long Tokens
+    */
+    /** "<string>"  */ String,
+    /** foo         */ Ident,
+    /** 123         */ Int,
+    /** 123.123     */ Float,
 }
 
 export
 type Token = {
-	pos:  number
-	kind: Token_Kind
+    pos:  number
+    kind: Token_Kind
 }
 
 export
 type Tokenizer = {
-	src      : string
-	pos_read : number
-	pos_write: number
+    src      : string
+    pos_read : number
+    pos_write: number
 }
 
 export
 const tokenizer_make = (src: string): Tokenizer => {
-	let t: Tokenizer = {
-		src:       src,
-		pos_read:  0,
-		pos_write: 0,
-	}
-	return t
+    let t: Tokenizer = {
+        src:       src,
+        pos_read:  0,
+        pos_write: 0,
+    }
+    return t
 }
 export
 const make_tokenizer = tokenizer_make
@@ -68,22 +68,22 @@ const make_tokenizer = tokenizer_make
 export
 const next_char_code = (t: Tokenizer): number => {
 
-	if (t.pos_read >= t.src.length) {
-		return 0
-	}
+    if (t.pos_read >= t.src.length) {
+        return 0
+    }
 
-	t.pos_read += 1
-	return char_code(t)
+    t.pos_read += 1
+    return char_code(t)
 }
 export
 const next_char = (t: Tokenizer): string => {
 
-	if (t.pos_read >= t.src.length) {
-		return ''
-	}
+    if (t.pos_read >= t.src.length) {
+        return ''
+    }
 
-	t.pos_read += 1
-	return char(t)
+    t.pos_read += 1
+    return char(t)
 }
 
 /** gets current char */
@@ -105,115 +105,115 @@ export const is_ident = (ch: string): boolean => is_ident_code(ch.charCodeAt(0))
 
 export
 const make_token = (t: Tokenizer, kind: Token_Kind): Token => {
-	let token: Token = {
-		pos:  t.pos_write,
-		kind: kind,
-	}
-	t.pos_write = t.pos_read
-	next_char(t)
-	return token
+    let token: Token = {
+        pos:  t.pos_write,
+        kind: kind,
+    }
+    t.pos_write = t.pos_read
+    next_char(t)
+    return token
 }
 export
 const make_token_go_back = (t: Tokenizer, kind: Token_Kind): Token => {
-	let token: Token = {
-		pos:  t.pos_write,
-		kind: kind,
-	}
-	t.pos_write = t.pos_read-1
-	return token
+    let token: Token = {
+        pos:  t.pos_write,
+        kind: kind,
+    }
+    t.pos_write = t.pos_read-1
+    return token
 }
 
 export
 const next_token = (t: Tokenizer): Token => {
-	
-	if (t.pos_read >= t.src.length) {
-		return make_token(t, Token_Kind.EOF)
-	}
+    
+    if (t.pos_read >= t.src.length) {
+        return make_token(t, Token_Kind.EOF)
+    }
 
-	let ch = char_code(t)
-	
-	switch (ch) {
-	// Whitespace
-	case 10 /* '\n' */:
-		t.pos_write = t.pos_read - 1
-		return make_token(t, Token_Kind.EOL)
-	case 32 /* ' '  */:
-	case 9  /* '\t' */: 
-	case 13 /* '\r' */:
-		t.pos_write = t.pos_read
-		next_char(t)
-		return next_token(t)
-	// Punctuators
-	case 40 /* '(' */: return make_token(t, Token_Kind.Paren_L)
-	case 41 /* ')' */: return make_token(t, Token_Kind.Paren_R)
-	// Operators
-	case 61 /* '=' */: return make_token(t, Token_Kind.Eq)
-	case 43 /* '+' */: return make_token(t, Token_Kind.Add)
-	case 45 /* '-' */: return make_token(t, Token_Kind.Sub)
-	case 42 /* '*' */: return make_token(t, Token_Kind.Mul)
-	case 47 /* '/' */: return make_token(t, Token_Kind.Div)
-	case 94 /* '^' */: return make_token(t, Token_Kind.Pow)
-	case 38 /* '&' */: return make_token(t, Token_Kind.And)
-	case 124/* '|' */: return make_token(t, Token_Kind.Or)
-	case 63 /* '?' */: return make_token(t, Token_Kind.Question)
-	case 64 /* '@' */: return make_token(t, Token_Kind.At)
-	// String
-	case 34 /* '"' */: {
+    let ch = char_code(t)
+    
+    switch (ch) {
+    // Whitespace
+    case 10 /* '\n' */:
+        t.pos_write = t.pos_read - 1
+        return make_token(t, Token_Kind.EOL)
+    case 32 /* ' '  */:
+    case 9  /* '\t' */: 
+    case 13 /* '\r' */:
+        t.pos_write = t.pos_read
+        next_char(t)
+        return next_token(t)
+    // Punctuators
+    case 40 /* '(' */: return make_token(t, Token_Kind.Paren_L)
+    case 41 /* ')' */: return make_token(t, Token_Kind.Paren_R)
+    // Operators
+    case 61 /* '=' */: return make_token(t, Token_Kind.Eq)
+    case 43 /* '+' */: return make_token(t, Token_Kind.Add)
+    case 45 /* '-' */: return make_token(t, Token_Kind.Sub)
+    case 42 /* '*' */: return make_token(t, Token_Kind.Mul)
+    case 47 /* '/' */: return make_token(t, Token_Kind.Div)
+    case 94 /* '^' */: return make_token(t, Token_Kind.Pow)
+    case 38 /* '&' */: return make_token(t, Token_Kind.And)
+    case 124/* '|' */: return make_token(t, Token_Kind.Or)
+    case 63 /* '?' */: return make_token(t, Token_Kind.Question)
+    case 64 /* '@' */: return make_token(t, Token_Kind.At)
+    // String
+    case 34 /* '"' */: {
 
-		let escaping = false
+        let escaping = false
 
-		// String
-		for (;;) {
-			switch (next_char_code(t)) {
-			case 0:
-			case 10 /* '\n' */:
-				return make_token_go_back(t, Token_Kind.Invalid)
-			case 92 /* '\\' */:
-				escaping = !escaping
-				break
-			case 34 /* '"' */:
-				if (!escaping) {
-					return make_token(t, Token_Kind.String)
-				}
-				escaping = false
-				break
-			default:
-				escaping = false
-			}
-		}
-	}
-	}
+        // String
+        for (;;) {
+            switch (next_char_code(t)) {
+            case 0:
+            case 10 /* '\n' */:
+                return make_token_go_back(t, Token_Kind.Invalid)
+            case 92 /* '\\' */:
+                escaping = !escaping
+                break
+            case 34 /* '"' */:
+                if (!escaping) {
+                    return make_token(t, Token_Kind.String)
+                }
+                escaping = false
+                break
+            default:
+                escaping = false
+            }
+        }
+    }
+    }
 
-	// Number
-	// 123
-	// 123.456
-	if (is_digit_code(ch)) {
+    // Number
+    // 123
+    // 123.456
+    if (is_digit_code(ch)) {
 
-		while (ch = next_char_code(t), is_digit_code(ch)) {
+        while (ch = next_char_code(t), is_digit_code(ch)) {
 
-			// fraction (123.456)
-			if (ch === 46 /* '.' */) {
+            // fraction (123.456)
+            if (ch === 46 /* '.' */) {
 
-				if (!is_digit_code(next_char_code(t))) {
-					return make_token_go_back(t, Token_Kind.Invalid)
-				}
+                if (!is_digit_code(next_char_code(t))) {
+                    return make_token_go_back(t, Token_Kind.Invalid)
+                }
 
-				while (is_digit_code(next_char_code(t))) {}
+                while (is_digit_code(next_char_code(t))) {}
 
-				return make_token_go_back(t, Token_Kind.Float)
-			}
-		}
+                return make_token_go_back(t, Token_Kind.Float)
+            }
+        }
 
-		return make_token_go_back(t, Token_Kind.Int)
-	}
+        return make_token_go_back(t, Token_Kind.Int)
+    }
 
-	// Identifiers
-	if (is_ident_code(ch)) {
-		while (is_ident_code(next_char_code(t))) {}
-		return make_token_go_back(t, Token_Kind.Ident)
-	}
-	
-	return make_token(t, Token_Kind.Invalid)
+    // Identifiers
+    if (is_ident_code(ch)) {
+        while (is_ident_code(next_char_code(t))) {}
+        return make_token_go_back(t, Token_Kind.Ident)
+    }
+    
+    return make_token(t, Token_Kind.Invalid)
 }
 
 /*
@@ -234,52 +234,52 @@ const UNARY_OPS = new Set([Token_Kind.Add, Token_Kind.Sub]);
 const BINARY_OPS = new Set([Token_Kind.Add, Token_Kind.Sub, Token_Kind.Mul, Token_Kind.Div, Token_Kind.Pow]);
 
 export type Expr =
-	| Expr_Ident
-	| Expr_Number
-	| Expr_Unary
-	| Expr_Binary
-	| Expr_Paren
-	| Expr_Comma
+    | Expr_Ident
+    | Expr_Number
+    | Expr_Unary
+    | Expr_Binary
+    | Expr_Paren
+    | Expr_Comma
 
 export type Expr_Ident = {
-	tok: Token
+    tok: Token
 }
 
 export type Expr_Number = {
-	tok: Token
+    tok: Token
 }
 
 export type Expr_Unary = {
-	op:  Token
-	rhs: Expr
+    op:  Token
+    rhs: Expr
 }
 
 export type Expr_Binary = {
-	op:  Token
-	lhs: Expr
-	rhs: Expr
+    op:  Token
+    lhs: Expr
+    rhs: Expr
 }
 
 export type Expr_Paren = {
-	type: Expr | null
-	body: Expr[]
-	lhs:  Token
-	rhs:  Token
+    type: Expr | null
+    body: Expr[]
+    lhs:  Token
+    rhs:  Token
 }
 
 export type Expr_Comma = {
-	tok: Token
+    tok: Token
 }
 
 export type Parser = {
-	src:   string,
-	t:     Tokenizer,
-	token: Token,
+    src:   string,
+    t:     Tokenizer,
+    token: Token,
 }
 
 export const parser_next_token = (p: Parser): Token => {
-	p.token = next_token(p.t)
-	return p.token
+    p.token = next_token(p.t)
+    return p.token
 }
 
 export const parse_src = (src: string): Expr[] => {
@@ -371,13 +371,13 @@ const parse_expr_atom = (p: Parser): Expr => {
 let input = `
 
 Counter = (
-	count ?= 1
-	double = count * 2
-	increment = @(count += 1)
-	render = Button(
-		text = "Count: " + count + ", Double: " + double
-		onclick = increment
-	)
+    count ?= 1
+    double = count * 2
+    increment = @(count += 1)
+    render = Button(
+        text = "Count: " + count + ", Double: " + double
+        onclick = increment
+    )
 )
 
 `
@@ -387,13 +387,13 @@ let t = tokenizer_make(input)
 
 let lines = ''
 for (;;) {
-	let tok = next_token(t)
+    let tok = next_token(t)
 
-	lines += `${Token_Kind[tok.kind]} `
+    lines += `${Token_Kind[tok.kind]} `
 
-	if (tok.kind === Token_Kind.EOL) lines += '\n'
-	
-	if (tok.kind === Token_Kind.EOF) break
+    if (tok.kind === Token_Kind.EOL) lines += '\n'
+    
+    if (tok.kind === Token_Kind.EOF) break
 }
 
 console.log(lines)
